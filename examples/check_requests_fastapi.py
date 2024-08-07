140f6655a56de6ad6d7f2deb8b29124e1aaef3fc
adalovelace3567 <ad3d13@gmail.com>
2024-08-07 19:08:33 +0900

    remove pydantic

--- examples/check_requests_fastapi.py
+++ examples/check_requests_fastapi.py
@@ -1,20 +1,13 @@
 import json
-from typing import Any, Dict, Optional
-from pydantic import BaseModel
+
+import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-import uvicorn
 
 from logger.custom_logger import logger
 from utils.parser import parse_form_data
 
 app = FastAPI()
-
-
-class RequestData(BaseModel):
-    headers: Optional[Dict[str, Any]]
-    query_params: Optional[Dict[str, str]]
-    body: Optional[Dict[str, Any]]
 
 
 @app.middleware("http")
@@ -27,21 +20,24 @@ async def log_request_data(request: Request, call_next):
 
     # Выводим заголовки в формате JSON
     headers = dict(request.headers)
+
     headers_json = json.dumps(headers, indent=4, ensure_ascii=False)
     logger_message.append(f"Заголовки запроса:\n{headers_json}")
 
     # Выводим параметры запроса
     query_params = dict(request.query_params)
+
     query_params_json = json.dumps(query_params, indent=4, ensure_ascii=False)
     logger_message.append(f"Параметры запроса:\n{query_params_json}")
 
     # Тип содержимого тела запроса
     content_type = request.headers.get("content-type")
-    body = None
+    body = {}
 
     # Если тело запроса в формате JSON
     if content_type == "application/json":
         body = await request.json()
+
         body_json = json.dumps(body, indent=4, ensure_ascii=False)
         logger_message.append(f"Тело запроса (JSON):\n{body_json}")
 
@@ -52,17 +48,16 @@ async def log_request_data(request: Request, call_next):
         body = parse_form_data(body)
 
         form_data_json = json.dumps(body, indent=4, ensure_ascii=False)
-
-        logger_message.append(f"Тело запроса (Form Data):\n{form_data_json}\n")
+        logger_message.append(f"Тело запроса (Form Data):\n{form_data_json}")
 
     # Логирование информации о запросе
     logger.debug("\n".join(logger_message))
     logger_message = []
 
     # Сохранение информации о запросе
-    request.state.data = RequestData(
-        headers=headers, query_params=query_params, body=body
-    )
+    request.state.headers = headers
+    request.state.query_params = query_params
+    request.state.body = body
 
     # Передача запроса обработчику
     response = await call_next(request)
@@ -105,7 +100,7 @@ async def install(request: Request):
 
 @app.post("/handler")
 async def handler(request: Request):
-    return request.state.data.body
+    return request.state.body
 
 
 if __name__ == "__main__":
