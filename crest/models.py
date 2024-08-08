52cf053dbc343557db92509b2dd14cc5cbbe9f47
Владислав <123343007+laga-vladislav@users.noreply.github.com>
2024-08-08 16:02:33 +1000

    добавил CallRequest
    для более удобной работы с запросами

---
+++ crest/models.py
@@ -0,0 +1,36 @@
+import urllib
+from pydantic import BaseModel
+from typing import Dict
+
+
+class CallRequest(BaseModel):
+    method: str
+    params: Dict = {}
+
+    def form_data(self, convention="%s"):
+        return self._format_params_recursion(self.params, convention)
+
+    def _format_params_recursion(self, params, convention="%s"):
+        if not params:
+            return ""
+
+        output = []
+        for key, value in params.items():
+            if isinstance(value, dict):
+                output.append(self._format_params_recursion(
+                    value, convention % key + "[%s]"))
+            elif isinstance(value, list):
+                new_params = {str(i): element for i,
+                              element in enumerate(value)}
+                output.append(self._format_params_recursion(
+                    new_params, convention % key + "[%s]"))
+            else:
+                key = urllib.parse.quote(key)
+                val = urllib.parse.quote(str(value))
+                output.append(convention % key + "=" + val)
+
+        return "&".join(output)
+
+    def get_path(self):
+        url = f'{self.method}?{self.form_data()}'
+        return url
